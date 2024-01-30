4bd2ba27e89027de229b273c7f1f8a8cdaec10da
sirouk <8901571+sirouk@users.noreply.github.com>
2024-01-30 01:12:37 -0500

    Update monitor_abuse.py
    nuked conntrack

--- monitor_abuse.py
+++ monitor_abuse.py
@@ -23,7 +23,6 @@ def ban_ip_in_ufw(ip):
     command = f"""
     while sudo netstat -an | grep ESTABLISHED | grep -q {ip}; 
     do 
-        sudo conntrack -D --orig-src {ip}; 
         sudo ss --kill -tn 'dst == {ip}'; 
         sleep 1; 
     done
