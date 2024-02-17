3fa4fdcb61f0eae29fea679e3773cea31f4b2a56
sirouk <8901571+sirouk@users.noreply.github.com>
2024-02-17 01:24:06 -0500

    Update monitor_abuse.py

--- monitor_abuse.py
+++ monitor_abuse.py
@@ -44,7 +44,7 @@ liveness_interval = 100 # Time in seconds to check for liveness (100 sec = 1min 
 auto_restart_process = True # Whether you want the script to restart the pm2 process if it is found without meaningful work past a period of time
 subnet_oldest_debug_minutes = { # Configuration for subnet-specific oldest debug axon minutes
     -1: 5,
-    13: 25,
+    13: 30,
     22: 15,
     # Add more as needed
 }
