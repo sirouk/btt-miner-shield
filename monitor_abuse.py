4ab837daeecf831f5e7707b25265687ba442d0e6
sirouk <8901571+sirouk@users.noreply.github.com>
2024-02-27 22:24:26 -0500

    Update monitor_abuse.py

--- monitor_abuse.py
+++ monitor_abuse.py
@@ -48,7 +48,7 @@ subnet_oldest_debug_minutes = { # Configuration for subnet-specific oldest debug
     17: 10,
     18: 5,
     19: 10,
-    22: 45,
+    22: 10,
     27: 10,
     # Add more as needed
 }
@@ -58,7 +58,7 @@ subnet_liveness_check_cmd = { # Dictionary mapping subnet IDs to grep commands f
     17: "grep -e 'DEBUG' | grep -ie 'Streamed t'",
     18: "grep -e 'INFO' | grep -ie 'Streamed t'",
     19: "grep -e 'DEBUG' | grep -ie '-->' | grep -ie 'AvailableOperations' | grep -ie 'Success'",
-    22: "grep -e 'INFO' | grep -ie 'Prompt Analysis'",
+    22: "grep -e 'INFO' | grep -ie 'answered to be active'",
     27: "grep -e 'SUCCESS' | grep -ie 'Challenge' | grep -ie 'seconds'",
     # Add more custom grep commands for other subnets as needed
 }
