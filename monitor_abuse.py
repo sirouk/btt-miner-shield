f4b759b3e3671976c460f86cb83e97fa9f33b6d7
sirouk <8901571+sirouk@users.noreply.github.com>
2024-03-20 14:16:05 -0400

    Update monitor_abuse.py

--- monitor_abuse.py
+++ monitor_abuse.py
@@ -47,7 +47,7 @@ subnet_oldest_debug_minutes = { # Configuration for subnet-specific oldest debug
     13: 15,
     17: 15,
     18: 5,
-    19: 10,
+    19: 30,
     22: 30,
     27: 15,
     31: 10,
