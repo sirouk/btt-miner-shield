adaeac542ee4a4e3bb79d0c9c8af44dc8f8d0795
sirouk <8901571+sirouk@users.noreply.github.com>
2024-04-20 12:04:18 -0400

    Update monitor_abuse.py
    SN19: less frequent restarts on DR miners

--- monitor_abuse.py
+++ monitor_abuse.py
@@ -48,7 +48,7 @@ subnet_oldest_debug_minutes = { # Configuration for subnet-specific oldest debug
     13: 15,
     17: 15,
     18: 5,
-    19: 10,
+    19: 15,
     22: 20,
     27: 15,
     31: 10,
